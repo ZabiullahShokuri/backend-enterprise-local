1b437e09aafb349c4db45d3d9d39a30517fc4e15
zabiullah-shokuri <zabiullah.shokuri.tripsi@gmail.com>
2025-12-15 20:59:12 +0330

    users: fix module definition and add users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: { email: string; password: string }) {
+    return this.prisma.user.create({ data });
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
