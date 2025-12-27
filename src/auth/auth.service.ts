3244f204fdc1ce346795631c775b88e19c4470cf
zabiullah-shokuri <zabiullah.shokuri.tripsi@gmail.com>
2025-12-27 21:01:37 +0330

    Save current progress before refactoring project

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(data: RegisterDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email already registered');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
